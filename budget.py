f188a0d84e4b8128dc3df44b883441634ef500a1
Owen Abbott <orabbott@gmail.com>
2024-09-24 15:38:57 -0500

    Adding programs

--- budget.py
+++ budget.py
@@ -1,34 +1,32 @@
 '''
 
 Program to assist Jayhawks in budgeting their dining dollars.
-daysleft defaults to 114, as this is the amount of days in the semester
+
 '''
 
-max = 0
 amount = 0
 
 while(True):
 	plan = input("Are you using the Crimson or Blue plan?: ")
 	if (plan.lower() == "crimson"):
-		max = 2170.00
+		amount = 2170.00
 		break
 	elif (plan.lower() == "blue"):
-		max = 2013.00
+		amount = 2013.00
 		break
 	else:
 		print('Please enter "Crimson" or "Blue". ')
-spent = input("Have you spent any dining dollars yet? (Y/N)").upper
+spent = input("Have you spent any dining dollars yet? (Y/N): ").upper()
 if spent == "Y" or spent == "YES":
-	amount = float(input("How many dining dollars do you currently have?"))
-else:
-	amount = max
-	
-def date_dif(month1, day1, year1, month2, day2, year2):
+	print("yes")
+	amount = float(input("How many dining dollars do you currently have?: "))
+
+def date_differ(month1, day1, year1, month2, day2, year2):
 	total = 0
 	curday = day1
 	curmon = month1
 	curyear = year1
-	def find_days(month, year):
+	def find_day(month, year):
 		if month == 4 or month == 6 or month == 9 or month == 11:
 			return 30
 		elif month == 2 and year % 4 == 0:
@@ -39,7 +37,7 @@ def date_dif(month1, day1, year1, month2, day2, year2):
 			return 31
 
 	while curday != day2 or curmon != month2 or curyear != year2:
-		while curday < find_days(curmon, curyear):
+		while curday < find_day(curmon, curyear):
 			curday += 1
 			total += 1
 			if curday == day2 and curmon == month2 and curyear == year2:
@@ -65,6 +63,7 @@ if date[3] == "0":
 	day = date[4]
 else:
 	day = date[3] + date[4]
-date_dif(mon, day, year, 12, 20, 2024)
 
+days = date_differ(int(mon), int(day), int(date[6] + date[7] + date[8] + date[9]), 12, 20, 2024)
 
+print("You should be spending about $" + str(round(amount / days, 2)) + " per day.")
